35fd69b5860daadf315244659feac5d3e92419b1
maxlio <phpmax@gmail.com>
2017-01-04 17:13:53 +0300

    fixing map issue 6

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,7 +43,6 @@
 //= require bootstrap_components
 //
 // ### Custom etensions
-//= require custom_ext/map
 //= require  ext/cmaps/cmaps
 //= require custom_ext/elements_select
 //= require custom_ext/handlebars
