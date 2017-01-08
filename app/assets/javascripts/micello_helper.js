e89583ec1ca3351785faee83217242481ff4b60b
maxlio <phpmax@gmail.com>
2017-01-08 18:02:14 +0300

    removing markers array

--- app/assets/javascripts/micello_helper.js
+++ app/assets/javascripts/micello_helper.js
@@ -29,14 +29,8 @@
       return currentlevel = this.mapObj.getCurrentLevel();
     };
 
-    micelloHelper.prototype.addMarker = function(markerId, posX, posY, lid, beacon_name, type) {
+    micelloHelper.prototype.addMarker = function(posX, posY, lid, beacon_name, type) {
       var marker, markersetting, thislevel;
-      if (markerId === null) {
-        return;
-      }
-      if (this._markers[markerId] !== void 0) {
-        return;
-      }
       thislevel = this.currentLevel();
       markersetting = this.markerstruct;
       markersetting.lid = thislevel.id;
@@ -44,8 +38,7 @@
       markersetting.mx = posX;
       markersetting.my = posY;
       markersetting.idat = beacon_name;
-      marker = this.mapObj.addMarkerOverlay(markersetting);
-      return this._markers[markerId] = marker;
+      return marker = this.mapObj.addMarkerOverlay(markersetting);
     };
 
     micelloHelper.prototype.clearAll = function() {
