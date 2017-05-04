96d71d68eed8a5e6c9d542ed3fe4763a455e5268
maxlio <phpmax@gmail.com>
2017-05-04 19:43:12 +0300

    Update rack_timeout.rb

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,1 +1,1 @@
-Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 100
+Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 200
